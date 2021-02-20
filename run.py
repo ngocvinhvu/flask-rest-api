59ca42612e50348043165197ed287e3d805c6173
trisduong <trisduong.tpi@gmail.com>
2021-02-20 19:51:51 +0700

    update dotenv

--- run.py
+++ run.py
@@ -3,8 +3,6 @@ from app import create_app, db
 from flask_migrate import Migrate
 from dotenv import load_dotenv
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-dotenv_path = os.path.join(basedir, '.env')
-load_dotenv(dotenv_path)
-app = create_app(os.getenv('FLASK_ENV') or 'default')
+load_dotenv()
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
