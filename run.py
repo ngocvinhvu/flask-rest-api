5ccd551db64a01306b8fab32b642ada23ee01ac7
Tris Duong <70458716+trisduong@users.noreply.github.com>
2021-02-23 09:10:25 +0700

    update

--- run.py
+++ run.py
@@ -2,8 +2,6 @@ import os
 from app import create_app, db
 from flask_migrate import Migrate
 from dotenv import load_dotenv
-import sys
-import click
 
 load_dotenv()
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
